baadf9d5187c2ac0a4c54a1c12f9afb6eba3b718
Prichlada Phadamnoen <prichlada.pha@dome.tu.ac.th>
2026-02-09 16:24:02 +0700

    9 Feb 2026

---
+++ week06/web_app/src/lib/Quantifier.svelte
@@ -0,0 +1,17 @@
+<script>
+let quantity = 0
+function increment() {
+quantity += 1
+}
+function decrement() {
+if (quantity === 0) {
+return
+}
+quantity -= 1
+}
+</script>
+<span>Current quantity: {quantity}</span>
+<div>
+<button class="button" onclick={decrement}>-</button>
+<button class="button" onclick={increment}>+</button>
+</div>
